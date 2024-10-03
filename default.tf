27455106ef0bbe2832e50329edf7bf59e4bbac47
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-10-03 10:06:11 +0530

    Update default.tf

--- default.tf
+++ default.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"  # Adjust the version as necessary
+      version = "~> 5.0"  # Adjust the version as necessary
     }
   }
 }
