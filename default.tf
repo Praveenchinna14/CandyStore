2dc719bc3249c00fec1a11edff641ada6f6d14a3
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-10-03 10:03:42 +0530

    Update default.tf

--- default.tf
+++ default.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Adjust the version as necessary
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
