1394ad13656fd674db70f6fde817968de60df693
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-10-04 11:15:55 +0530

    Update default.tf

--- default.tf
+++ default.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region = "ap-south-1"
-withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+withCredentials([aws(accessKeyVariable: "AWS_ACCESS_KEY_ID", credentialsId: "aws-cred", secretKeyVariable: "AWS_SECRET_ACCESS_KEY")]) {
     // some block
+   }
 }
 
 resource "aws_instance" "dev" {
